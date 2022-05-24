b4dbc0c03377fe318cda60242131f751e81d9c5c
bason1515 <bason1515@gmail.com>
2022-05-24 12:55:54 +0200

    Lecture 11 Exercises

--- src/main/scala/com/evolutiongaming/bootcamp/typeclass/v3_typeclass/TypeClasses.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/typeclass/v3_typeclass/TypeClasses.scala
@@ -35,76 +35,89 @@ object FPJson extends App {
 
   prettyPrint(Game(123))
 
-  object InstancesTask {
+  object InstancesTask { // exercise
 
     final case class Player(id: Int, name: String)
 
-    implicit val playerJsonable: Jsonable[Player] = ???
+    implicit val playerJsonable: Jsonable[Player] = new Jsonable[Player] {
+      override def toJson(player: Player): Json =
+        Json(
+          s"""
+             |{
+             | "id":${player.id}
+             | "name":${player.name}
+             |}
+             |""".stripMargin
+        )
+    }
 
-    implicit val intJsonable: Jsonable[Int] = ???
+    implicit val intJsonable: Jsonable[Int] = (e: Int) => Json(e.toString)
 
-    implicit val optionIntJsonable: Jsonable[Option[Int]] = ???
+    implicit val optionIntJsonable: Jsonable[Option[Int]] = (e: Option[Int]) => e match {
+      case Some(value) => Json(value.toString)
+      case None        => Json("null")
+    }
   }
 
   object GenericImplicitsTask {
 
     implicit def optionJsonable[A](implicit jsonableA: Jsonable[A]): Jsonable[Option[A]] =
-      new Jsonable[Option[A]] {
-        def toJson(entity: Option[A]): Json =
-          entity match {
-            case Some(value) => jsonableA.toJson(value)
-            case None        => Json("null")
-          }
+      (entity: Option[A]) => entity match {
+        case Some(value) => jsonableA.toJson(value)
+        case None        => Json("null")
       }
 
-    implicit def listJsonable[A](implicit jsonableA: Jsonable[A]): Jsonable[List[A]] = ???
-  }
+    implicit def listJsonable[A](implicit jsonableA: Jsonable[A]): Jsonable[List[A]] =
+      (l: List[A]) => Json(l.map(jsonableA.toJson).mkString("[", ", ", "]"))
 
-  object SingleAbstractMethod {
+    object SingleAbstractMethod {
 
-    implicit val before: Jsonable[Game] = new Jsonable[Game] {
-      def toJson(game: Game): Json = Json(s"{${'"'}id${'"'}:${game.id}}")
-    }
-
-    implicit val after: Jsonable[Game] = ???
-  }
-
-  object ContextBound {
-
-    def prettyPrintBefore[A](a: A)(implicit jsonable: Jsonable[A]): Unit = println(jsonable.toJson(a))
-
-    def prettyPrintAfter[A: Jsonable](a: A): Unit = ???
-  }
-
-  object Summoner {
-
-    object Jsonable {
-      def apply[A](implicit instance: Jsonable[A]): Jsonable[A] = instance
-    }
-
-    def prettyPrintBefore[A: Jsonable](a: A): Unit = {
-      val jsonable = implicitly[Jsonable[A]]
-      println(jsonable.toJson(a))
-    }
-
-    def prettyPrintWithSummoner[A: Jsonable](a: A): Unit = ???
-  }
-
-  object Syntax {
-
-    object Jsonable {
-      def apply[A](implicit instance: Jsonable[A]): Jsonable[A] = instance
-    }
-
-    def prettyPrintBefore[A: Jsonable](a: A): Unit = println(Jsonable[A].toJson(a))
-
-    object JsonableSyntax {
-      implicit class JsonableOps[A](x: A) {
-        def toJson(implicit jsonable: Jsonable[A]): Json = jsonable.toJson(x)
+      implicit val before: Jsonable[Game] = new Jsonable[Game] {
+        def toJson(game: Game): Json = Json(s"{${'"'}id${'"'}:${game.id}}")
       }
+
+      implicit val after: Jsonable[Game] = (g: Game) => Json(s"{${'"'}id${'"'}:${g.id}}")
     }
 
-    def prettyPrintWithSyntax[A: Jsonable](a: A): Unit = ???
+    object ContextBound {
+
+      def prettyPrintBefore[A](a: A)(implicit jsonable: Jsonable[A]): Unit = println(jsonable.toJson(a))
+
+      def prettyPrintAfter[A: Jsonable](a: A): Unit = println(implicitly[Jsonable[A]].toJson(a))
+    }
+
+    object Summoner {
+
+      object Jsonable {
+        def apply[A](implicit instance: Jsonable[A]): Jsonable[A] = instance
+      }
+
+      def prettyPrintBefore[A: Jsonable](a: A): Unit = {
+        val jsonable = implicitly[Jsonable[A]]
+        println(jsonable.toJson(a))
+      }
+
+      def prettyPrintWithSummoner[A: Jsonable](a: A): Unit = println(Jsonable[A].toJson(a))
+    }
+
+    object Syntax {
+
+      object Jsonable {
+        def apply[A](implicit instance: Jsonable[A]): Jsonable[A] = instance
+      }
+
+      def prettyPrintBefore[A: Jsonable](a: A): Unit = println(Jsonable[A].toJson(a))
+
+      object JsonableSyntax {
+        implicit class JsonableOps[A](x: A) {
+          def toJson(implicit jsonable: Jsonable[A]): Json = jsonable.toJson(x)
+        }
+      }
+
+      import JsonableSyntax._
+
+      def prettyPrintWithSyntax[A: Jsonable](a: A): Unit = println(a.toJson)
+    }
   }
 }
 
@@ -125,6 +138,7 @@ object FPJsonSugared extends App {
   }
 
   import JsonableSyntax._
+
   def prettyPrint[A: Jsonable](a: A): Unit = println(a.toJson)
 
   final case class Game(id: Int)
@@ -134,37 +148,40 @@ object FPJsonSugared extends App {
   prettyPrint(Game(123))
 }
 
-//object FPJsonMacros extends App {
-//  import simulacrum._
-//
-//  @typeclass trait Jsonable[T] {
-//    def toJson(entity: T): Json
-//  }
-//
-//  import Jsonable.ops._
-//  def prettyPrint[A: Jsonable](a: A): Unit = println(a.toJson)
-//
-//  final case class Game(id: Int)
-//
-//  implicit val gameJsonable: Jsonable[Game] = (game: Game) => Json(s"{${'"'}id${'"'}:${game.id}}")
-//
-//  prettyPrint(Game(123))
-//}
+object FPJsonMacros extends App {
 
-object HashCodeTask {
+  import simulacrum._
 
-  trait HashCode { // Turn me into TypeClass
-    def hash: Int
+  @typeclass trait Jsonable[T] {
+    def toJson(entity: T): Json
+  }
+
+  import Jsonable.ops._
+
+  def prettyPrint[A: Jsonable](a: A): Unit = println(a.toJson)
+
+  final case class Game(id: Int)
+
+  implicit val gameJsonable: Jsonable[Game] = (game: Game) => Json(s"{${'"'}id${'"'}:${game.id}}")
+
+  prettyPrint(Game(123))
+}
+
+object HashCodeTask extends App {
+
+  trait HashCode[T] { // Turn me into TypeClass
+    def hash(entity: T): Int
   }
 
   object HashCode {
-    // Implement a summoner for me
+    def apply[A](implicit instance: HashCode[A]): HashCode[A] = instance
   }
 
+  // Implement syntax so I could do "abc".hash
   implicit class HashCodeOps[A](x: A) {
-    // Implement syntax so I could do "abc".hash
+    def hash(implicit h: HashCode[A]): Int = h.hash(x)
   }
 
-  // Implement an instance for String
-  // Prove that I'm working
+  implicit val strHash: HashCode[String] = (entity: String) => entity.hashCode
+  println("abc.".hashCode)
 }
--- src/main/scala/com/evolutiongaming/bootcamp/typeclass/v3_typeclass/TypeClassesExamples.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/typeclass/v3_typeclass/TypeClassesExamples.scala
@@ -8,48 +8,93 @@ object TypeClassesExamples extends App {
     def combine(x: A, y: A): A
   }
 
-  // 1.2. Implement Semigroup for Long, String
+  object Semigroup {
+    def apply[A](implicit instance: Semigroup[A]): Semigroup[A] = instance
+  }
+
+  // 1.2. Implement Semigroup for Int, String
+  implicit val intSemigroup: Semigroup[Int] = (x: Int, y: Int) => x + y
+  implicit val stringSemigroup: Semigroup[String] = (x: String, y: String) => x + y
 
   // 1.3. Implement combineAll(list: List[A]) for non-empty lists
+  def combineAll[A: Semigroup](list: List[A]): A = list.reduce(Semigroup[A].combine)
 
-  // combineAll(List(1, 2, 3)) == 6
+  //  combineAll(List(1, 2, 3)) == 6
 
   // 1.4. Implement combineAll(list: List[A], startingElement: A) for all lists
+  def combineAll[A: Semigroup](list: List[A], startingElement: A): A =
+    list.foldLeft(startingElement)(Semigroup[A].combine)
 
-  // combineAll(List(1, 2, 3), 0) == 6
-  // combineAll(List(), 1) == 1
+  //  println(combineAll(List(1, 2, 3), 0) == 6)
+  //  println(combineAll(List(), 1) == 1)
 
   // 2. Monoid
   // 2.1. Implement Monoid which provides `empty` value (like startingElement in previous example) and extends Semigroup
+  trait Monoid[A] extends Semigroup[A] {
+    def empty: A
+  }
 
-  // 2.2. Implement Monoid for Long, String
+  object Monoid {
+    def apply[A](implicit instance: Monoid[A]): Monoid[A] = instance
+  }
+
+  // 2.2. Implement Monoid for Int, String
+  implicit val intMonoid: Monoid[Int] = new Monoid[Int] {
+    override def empty: Int = 0
+
+    override def combine(x: Int, y: Int): Int = x + y
+  }
+
+  implicit val stringMonoid: Monoid[String] = new Monoid[String] {
+    override def empty: String = ""
+
+    override def combine(x: String, y: String): String = x + y
+  }
 
   // 2.3. Implement combineAll(list: List[A]) for all lists
+  def combineAllMonoid[A: Monoid](list: List[A]): A = list.foldLeft(Monoid[A].empty)(Monoid[A].combine)
 
-  // combineAll(List(1, 2, 3)) == 6
+  //  println(combineAllMonoid(List(1, 2, 3)) == 6)
 
   // 2.4. Implement Monoid for Option[A]
+  implicit def optionMonoid[A: Monoid]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    override def empty: Option[A] = None
 
-  // combineAll(List(Some(1), None, Some(3))) == Some(4)
-  // combineAll(List(None, None)) == None
-  // combineAll(List()) == None
+    override def combine(x: Option[A], y: Option[A]): Option[A] = (x, y) match {
+      case (Some(x), Some(y)) => Some(Monoid[A].combine(x, y))
+      case _                  => x.orElse(y)
+    }
+  }
+
+  //  println(combineAllMonoid(List(Some(1), None, Some(3))).contains(4))
+  //  println(combineAllMonoid(List[Option[Int]](None, None)).isEmpty)
+  //  println(combineAllMonoid(List.empty[Option[Int]]).isEmpty)
 
   // 2.5. Implement Monoid for Function1 (for result of the function)
 
+  implicit def functionMonoid[T, R: Monoid]: Monoid[T => R] = new Monoid[T => R] {
+    override def empty: T => R = _ => Monoid[R].empty
+
+    override def combine(x: T => R, y: T => R): T => R = t => Monoid[R].combine(x(t), y(t))
+  }
+
   // combineAll(List((a: String) => a.length, (a: String) => a.toInt))        === (a: String) => (a.length + a.toInt)
-  // combineAll(List((a: String) => a.length, (a: String) => a.toInt))("123") === 126
+  println(combineAll(List(
+    (a: String) => a.length,
+    (a: String) => a.toInt)
+  ).apply("123"))
 
   // 3. Functor
   trait Functor[F[_]] {
     def map[A, B](fa: F[A])(f: A => B): F[B]
   }
 
-  implicit class FunctorOps[F[_]: Functor, A](fa: F[A]) {
+  implicit class FunctorOps[F[_] : Functor, A](fa: F[A]) {
     def map[B](f: A => B): F[B] = Functor[F].map(fa)(f)
   }
 
   object Functor {
-    def apply[F[_]: Functor]: Functor[F] = implicitly[Functor[F]]
+    def apply[F[_] : Functor]: Functor[F] = implicitly[Functor[F]]
   }
 
   implicit val optionFunctor: Functor[Option] = new Functor[Option] {
@@ -59,14 +104,34 @@ object TypeClassesExamples extends App {
   // 4. Semigroupal
   // 4.1. Implement Semigroupal which provides `product` method,
   // so in combination with Functor we'll be able to call for example `plus` on two Options (its content)
-  trait Semigroupal[F[_]]
+  trait Semigroupal[F[_]] {
+    def product[A, B](fa: F[A], fb: F[B]): F[(A, B)]
+  }
 
-  // 4.2. Implement Semigroupal for Option
+  object Semigroupal {
+    def apply[F[_] : Semigroupal]: Semigroupal[F] = implicitly[Semigroupal[F]]
+  }
+
+  // 4.2. Implement Semigroupal for Option (EX)
+  implicit val optionSemigroupal: Semigroupal[Option] = new Semigroupal[Option] {
+    override def product[A, B](fa: Option[A], fb: Option[B]): Option[(A, B)] = for {
+      a <- fa
+      b <- fb
+    } yield (a, b)
+  }
+
 
   // 4.3. Implement `mapN[R](f: (A, B) => R): F[R]` extension method for Tuple2[F[A], F[B]]
+  implicit class TupleOps[F[_], A, B](tuple: (F[A], F[B])) {
+    def mapN[R](f: (A, B) => R)(implicit semigroupal: Semigroupal[F], functor: Functor[F]): F[R] =
+      functor.map(semigroupal.product(tuple._1, tuple._2)) {
+        case (a, b) => f(a, b)
+      }
+  }
 
-  // (Option(1), Option(2)).mapN(_ + _) == Some(3)
-  // (Option(1), None).mapN(_ + _)      == None
+  //   println((Option(1), Option(2)).mapN(_ + _).contains(3))
+  //   println((Option(1), Option.empty[Int]).mapN(_ + _).isEmpty)
+
 
   // 4.4. Implement Semigroupal for Map
 
@@ -78,9 +143,25 @@ object TypeClassesExamples extends App {
   }
 
   // 5.1. Implement Applicative for Option, Either
+  implicit val optionApplicative: Applicative[Option] = new Applicative[Option] {
+    override def pure[A](x: A): Option[A] = Some(x)
+
+    override def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa.map(f)
+
+    override def product[A, B](fa: Option[A], fb: Option[B]): Option[(A, B)] = for {
+      a <- fa
+      b <- fb
+    } yield (a, b)
+  }
 
   // 5.2. Implement `traverse` for all Applicatives instead of Option
-  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = ???
+  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] =
+    as.foldLeft(Option(List.empty[B])) { (result, a) =>
+      result match {
+        case Some(value) => f(a).map(_ :: value).orElse(Option.empty[List[B]])
+        case None        => Option.empty[List[B]]
+      }
+    }
 
   // traverse(List(1, 2, 3)) { i =>
   //   Option.when(i % 2 == 1)(i)
